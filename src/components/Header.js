d9b21b1a55b194199648912d29e1238db429ee59
Hatim <hatimhaiderali786@gmail.com>
2024-08-15 01:45:11 +0500

    added name

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <header>
-      <h1>Your Name</h1>
+      <h1>Hatim Haider Ali</h1>
       <nav>
         <a href="#about">About</a>
         <a href="#projects">Projects</a>
